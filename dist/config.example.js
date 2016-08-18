18c187f97bd54907cf55c21671d574765d84e2af
Chloe Pou-Prom <chloe@smooch.io>
2016-08-18 15:14:46 -0400

    Fix gitignore issues

--- dist/config.example.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// Add in your keys and rename this file as 'config.js'
-
-var KEY_ID = 'ADD YOUR SMOOCH KEY_ID';
-var SECRET = 'ADD YOUR SMOOCH SECRET ID';
-var YANDEX_KEY = 'YANDEX API KEY';
-var APP_MAKER_LANGUAGE = 'en'; // Default to English
